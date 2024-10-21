4942a261e4770f67110ac71eef0a9f01eba8e89d
Grexzlyyy <163362279+Waahyuuu@users.noreply.github.com>
2024-10-21 14:11:25 +0700

    pertemuan 5
    y

---
+++ app/src/main/java/com/example/wahyuproject/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.wahyuproject
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun submit(view: View) {
+        val nameYOU = findViewById<EditText>(R.id.you_name)
+        val name = nameYOU.text.toString()
+        val toast = Toast.makeText(this, name, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+}
